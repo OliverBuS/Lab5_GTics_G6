4f7051aa6482f042bcaef5d350e4e4dc6cd628e9
Oliver <oliver.bustamante@pucp.edu.pe>
2022-05-03 23:02:25 -0500

    CarritoController

--- src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/CarritoController.java
+++ src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/CarritoController.java
@@ -1,11 +1,8 @@
-package edu.pucp.gtics.lab5_gtics_20221.controller;
+package pe.edu.pucp.teleticket;
 
 import edu.pucp.gtics.lab5_gtics_20221.entity.*;
 import edu.pucp.gtics.lab5_gtics_20221.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,19 +39,53 @@ public class CarritoController {
         }
         juegosxUsuarioRepository.saveAll(listaComprar);
         session.setAttribute("carrito", new ArrayList<Juegos>());
+        session.setAttribute("ncarrito", 0);
         return "redirect:/vista";
     }
 
+
     @GetMapping("/carrito/borrar")
-    public String borrarCarrito(@RequestParam("id") String idString){
+    public String borrarCarrito(HttpSession session,@RequestParam("id") String idString){
+        int id;
         try{
-            int id = Integer.parseInt(idString);
+            id = Integer.parseInt(idString);
         }catch (Exception e){
             e.printStackTrace();
             return "redirect:/vista";
         }
+        Optional<Juegos> optionalJuegos = juegosRepository.findById(id);
+        if(optionalJuegos.isEmpty()){return "redirect:/vista";}
+        List<Juegos> carrito = (List<Juegos>) session.getAttribute("carrito");
+        int ncarrito = (int) session.getAttribute("ncarrito");
+        int index=0;
+        for (Juegos juego : carrito){
+            if(juego.getIdjuego()==optionalJuegos.get().getIdjuego()){
+                carrito.remove(index);
+                ncarrito--;
+            }
+            index++;
+        }
+        session.setAttribute("ncarrito", ncarrito);
+        session.setAttribute("carrito",carrito);
+        return "redirect:/carrito/lista";
+    }
 
-
+    @GetMapping("/carrito/anadir")
+    public String anadirCarrito(HttpSession session, @RequestParam("id") String idString){
+        int id;
+        try {
+            id=Integer.parseInt(idString);
+        } catch (Exception e){
+            e.printStackTrace();
+            return "redirect:/vista";
+        }
+        Optional<Juegos> optionalJuegos = juegosRepository.findById(id);
+        if(optionalJuegos.isEmpty()){return "redirect:/vista";}
+        List<Juegos> carrito = (List<Juegos>) session.getAttribute("carrito");
+        int ncarrito = (int) session.getAttribute("ncarrito");
+        carrito.add(optionalJuegos.get());
+        session.setAttribute("ncarrito", ncarrito++);
+        session.setAttribute("carrito",carrito);
         return "redirect:/carrito/lista";
     }
 
