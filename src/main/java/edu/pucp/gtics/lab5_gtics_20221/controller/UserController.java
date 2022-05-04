b718e241d24e3d008b498958bb23c92e93c02e2d
Diego Vargas <a20191027@pucp.edu.pe>
2022-05-03 22:32:49 -0500

    navbar avance

--- src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/UserController.java
+++ src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -23,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
 
@@ -34,7 +36,7 @@ public class UserController {
         return "redirect:/product";
     }
 
-    @GetMapping("/user/singIn")
+    @GetMapping("/singIn")
     public String login(){
         return "user/signIn";
     }
