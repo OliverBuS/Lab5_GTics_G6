b24e98ec2677e44f21359f0c974718e7c8a78e1f
Oliver <oliver.bustamante@pucp.edu.pe>
2022-05-03 21:50:11 -0500

    Avance carrito

--- src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/CarritoController.java
+++ src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/CarritoController.java
@@ -5,6 +5,7 @@ import edu.pucp.gtics.lab5_gtics_20221.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,27 +16,44 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-
+@Controller
 public class CarritoController {
 
+    @Autowired
+    JuegosRepository juegosRepository;
+    @Autowired
+    JuegosxUsuarioRepository juegosxUsuarioRepository;
 
-
-    public String listaCarrito (Model model, ...){
-
+    @GetMapping("/carrito/lista")
+    public String listaCarrito (Model model, HttpSession session ){
         return "carrito/lista";
     }
 
-    public String nuevoCarrito(@RequestParam("id") int id, ...){
-
+    @GetMapping("/carrito/compra")
+    public String comprarCarrito(HttpSession session){
+        List<Juegos> carrito = (List<Juegos>) session.getAttribute("carrito");
+        User user = (User) session.getAttribute("user");
+        List<JuegosxUsuario> listaComprar = new ArrayList<JuegosxUsuario>();
+        for (Juegos i : carrito){
+            JuegosxUsuario juegosxUsuario = new JuegosxUsuario();
+            juegosxUsuario.setIdjuego(i);
+            juegosxUsuario.setIdusuario(user);
+            listaComprar.add(juegosxUsuario);
+        }
+        juegosxUsuarioRepository.saveAll(listaComprar);
+        session.setAttribute("carrito", new ArrayList<Juegos>());
         return "redirect:/vista";
     }
 
-    public String editarCarrito( ... ){
+    @GetMapping("/carrito/borrar")
+    public String borrarCarrito(@RequestParam("id") String idString){
+        try{
+            int id = Integer.parseInt(idString);
+        }catch (Exception e){
+            e.printStackTrace();
+            return "redirect:/vista";
+        }
 
-        return "redirect:/juegos/lista";
-    }
-
-    public String borrarCarrito(...){
 
         return "redirect:/carrito/lista";
     }
--- src/main/java/edu/pucp/gtics/lab5_gtics_20221/entity/JuegosxUsuario.java
+++ src/main/java/edu/pucp/gtics/lab5_gtics_20221/entity/JuegosxUsuario.java
@@ -1,10 +1,56 @@
 package edu.pucp.gtics.lab5_gtics_20221.entity;
 
 import javax.persistence.*;
-import java.io.Serializable;
 
-
-
+@Entity
+@Table(name = "juegosxusuario")
 public class JuegosxUsuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idjuegosxusuario", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "idjuego", nullable = false)
+    private Juegos idjuego;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "idusuario", nullable = false)
+    private User idusuario;
+
+    @Column(name = "cantidad")
+    private Integer cantidad;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Juegos getIdjuego() {
+        return idjuego;
+    }
+
+    public void setIdjuego(Juegos idjuego) {
+        this.idjuego = idjuego;
+    }
+
+    public User getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(User idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
 }
